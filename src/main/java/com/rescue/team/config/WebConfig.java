6f54ce3f8e580c96d43a7134c16b37a1f7d59df8
saligialin <3027862603@qq.com>
2021-04-08 17:16:00 +0800

    web配置类

---
+++ src/main/java/com/rescue/team/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.rescue.team.config;
+
+import com.rescue.team.filter.CORSInterceptor;
+import com.rescue.team.filter.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private CORSInterceptor corsInterceptor;
+
+    @Autowired
+    private RequestInterceptor requestInterceptor;
+
+    String[] loginPaths=new String[]{
+            "/**"
+    };
+    String[] unLoginPaths=new String[]{
+            "/user/getMsgCode",
+            "/user/loginByPassword",
+            "/user/loginByMessage",
+            "/user/register",
+            "/user/refreshToken",
+            "/error",
+            "/static/**",
+            "/webjars/**",
+            "/swagger-resources/**",
+            "/v2/api-docs",
+            "/swagger-ui.html"
+    };
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(corsInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(requestInterceptor).addPathPatterns(loginPaths).excludePathPatterns(unLoginPaths);
+    }
+}
