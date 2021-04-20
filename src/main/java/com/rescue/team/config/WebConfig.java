51985b3d6c6bc45b38849e978c3dc4f86f36c6b0
saligialin <3027862603@qq.com>
2021-04-20 14:42:25 +0800

    添加knife4j文档

--- src/main/java/com/rescue/team/config/WebConfig.java
+++ src/main/java/com/rescue/team/config/WebConfig.java
@@ -32,6 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
             "/swagger-resources/**",
             "/v2/api-docs",
             "/swagger-ui.html",
+            "/doc.html",
             "**/websocket/**",
             "/third/**",
             "/face/compared"
